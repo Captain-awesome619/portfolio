76d7b24bb502f62273975a214393d415010ebb4d
unknown <ogunsolatoluwalase@gmail.com>
2022-04-19 13:26:07 +0100

    first commit

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import React from 'react'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-    <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-    <a href="https://github.com" target="_blank"><FaGithub/></a>
-    <a href="https://dribble.com" target="_blank"><FiDribbble/></a>
+    <a href="https://linkedin.com" target="_blank" rel='noreferrer'><BsLinkedin/></a>
+    <a href="https://github.com" target="_blank" rel='noreferrer'><FaGithub/></a>
+    <a href="https://dribble.com" target="_blank" rel='noreferrer'><FiDribbble/></a>
     </div>
   )
 }
