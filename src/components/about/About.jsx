f07179d22b60530871887c9621df7a06dc7460aa
unknown <ogunsolatoluwalase@gmail.com>
2022-04-19 16:20:08 +0100

    first commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -33,7 +33,7 @@ const About = () => {
             <article className="about__card">
               <FaAward className="about__icon"/>
               <h5>Experience</h5>
-              <small>1 Year+ Web-Development Experience</small>
+              <small>2 Year+ Web-Development Experience</small>
               </article>
             <article className="about__card">
               < SiBookstack className="about__icon"/>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#experience">Expeirience</a></li>
+        <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#contact">Contact</a></li>
