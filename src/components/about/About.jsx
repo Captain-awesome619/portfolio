9e6677fa8dd1c1fd34b26fef8485d4fb80a08fc3
unknown <ogunsolatoluwalase@gmail.com>
2022-05-02 15:02:19 +0100

    inital commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -33,7 +33,7 @@ const About = () => {
             <article className="about__card">
               <FaAward className="about__icon"/>
               <h5>Experience</h5>
-              <small>2 Years+ Web-Development Experience</small>
+              <small>3 Years+ Web-Development Experience</small>
               </article>
             <article className="about__card">
               < SiBookstack className="about__icon"/>
