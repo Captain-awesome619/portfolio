5b179981a3c5090caa01527667a7777e93f80a09
unknown <ogunsolatoluwalase@gmail.com>
2022-07-24 18:54:10 +0100

    initial commit

--- src/components/animation.js
+++ src/components/animation.js
@@ -1,9 +1,9 @@
- export const experienceanimations = { 
+ export const experienceanimations = {
     hidden: {x:-200, opacity:0},
     show:  {x:0, opacity:1}
 }
- export const Aboutanimation = { 
-    hidden: {x:-200, opacity:0},
-    show:  {x:0, opacity:1}
+ export const Aboutanimation = {
+    hidden: {x:-200},
+    show:  {x: 0}
 }
 
