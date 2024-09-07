e16322a536b8effd14fcb92efdd25d60c2df4eaf
unknown <ogunsolatoluwalase@gmail.com>
2024-09-07 20:12:18 +0100

    heading

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -27,7 +27,7 @@ const data = [
   {
     id: 8,
     image: IMG8,
-    title: 'A Fullstack Mobile application that lets you upload and watch Videos',
+    title: 'A Fullstack Android Mobile application that lets you upload and watch Videos',
     github: 'https://github.com/Captain-awesome619/awesometube-mobile-App',
     demo: Apk
 
